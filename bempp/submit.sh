5b35ea8db99633d32af888610009e84fa32db57b
Philippe <philippe.marchner23@gmail.com>
2018-07-04 13:54:39 +0200

    update bempp

--- bempp/submit.sh
+++ bempp/submit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 python3 test.py 100
 echo job finished
-# use "time ./submit.sh" to run the job,662.5 412.5 pour res
+# use "time ./submit.sh" to run the job
+# resonant case: 662.5 412.5 Hz
+# non-resonant case: 430, 687.5 Hz
